e4e725452c5ad1c150caa66bbaa86e18e6a113b4
liuxudong-yonyou <dong1987>
2017-12-08 17:03:57 +0800

    重新初始化eureka监控项目

--- ops-service/src/main/java/com/yonyou/cloud/ops/service/controller/OpsServiceController.java
+++ ops-service/src/main/java/com/yonyou/cloud/ops/service/controller/OpsServiceController.java
@@ -1,37 +1,34 @@
-//package com.yonyou.cloud.ops.service.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.yonyou.cloud.common.annotation.YcApi;
-//import com.yonyou.cloud.common.beans.RestResultResponse;
-//import com.yonyou.cloud.common.controller.BaseController;
-//import com.yonyou.cloud.ops.service.entity.EurekaApplications;
-//import com.yonyou.cloud.ops.service.entity.InstanceInfo;
-//import com.yonyou.cloud.ops.service.service.OpsServiceService;
-//
-//
-//@RestController
-//public class OpsServiceController {
-//	
-//	@Autowired
-//	OpsServiceService opsServiceService;
-//	
-//	@RequestMapping(value="all",method=RequestMethod.GET)
-//	@YcApi
-//	public RestResultResponse<EurekaApplications> getAll(){
-//		return  new RestResultResponse<EurekaApplications>().data(opsServiceService.getAll()).success(true);
-//	}
-//	
-//	@RequestMapping(value="delete",method=RequestMethod.DELETE)
-//	@YcApi
-//	public RestResultResponse<EurekaApplications> deleteOne(){
-//		return  new RestResultResponse<EurekaApplications>().data(opsServiceService.getAll()).success(true);
-//	}
-//	
-//	
-//	
-//	
-//}
+package com.yonyou.cloud.ops.service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yonyou.cloud.common.annotation.YcApi;
+import com.yonyou.cloud.common.beans.RestResultResponse;
+import com.yonyou.cloud.common.controller.BaseController;
+import com.yonyou.cloud.ops.service.entity.EurekaApplications;
+import com.yonyou.cloud.ops.service.entity.InstanceInfo;
+import com.yonyou.cloud.ops.service.service.OpsServiceService;
+
+
+@RestController
+public class OpsServiceController {
+	
+	@Autowired
+	OpsServiceService opsServiceService;
+	
+	@RequestMapping(value="all",method=RequestMethod.GET)
+	@YcApi
+	public RestResultResponse<EurekaApplications> getAll(){
+		return  new RestResultResponse<EurekaApplications>().data(opsServiceService.getAll()).success(true);
+	}
+	
+	@RequestMapping(value="delete",method=RequestMethod.DELETE)
+	@YcApi
+	public RestResultResponse<EurekaApplications> deleteOne(){
+		return  new RestResultResponse<EurekaApplications>().data(opsServiceService.getAll()).success(true);
+	}
+	
+}
--- ops-service/src/main/java/com/yonyou/cloud/ops/service/mapper/InstanceInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yonyou.cloud.ops.service.mapper;
-
-import com.yonyou.cloud.ops.service.entity.InstanceInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface InstanceInfoMapper extends Mapper<InstanceInfo> {
-}
